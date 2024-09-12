a4dd3f9da0f52fd4e60abd36a8e59bb4634f416e
Syed-Ali-Jawad <alijawad04@gmail.com>
2024-09-12 17:39:17 +0500

    experince css adjustment

--- src/Components/AddExperience.jsx
+++ src/Components/AddExperience.jsx
@@ -6,67 +6,24 @@ export default function AddExerience({
   description,
   id,
 }) {
-  if (window.innerWidth > 768) {
-    if (id % 2 !== 0) {
-      return (
-        <div className="experience-div">
-          <div className="company" style={{ textAlign: "right" }}>
-            <img src={logo} />
-            <h3>{company}</h3>
-            <p>{periodServed}</p>
-          </div>
-          <div className="step">
-            <div className="outer-circle">
-              <div className="inner-circle"></div>
-            </div>
-            <hr className="vertical-line" />
-          </div>
-          <div className="responsibilities" style={{ textAlign: "left" }}>
-            <h3 style={{ textAlign: "start" }}>{positon}</h3>
-            <p style={{ textAlign: "left" }}>{description}</p>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="experience-div">
-          <div className="responsibilities" style={{ textAlign: "right" }}>
-            <h3 style={{ textAlign: "end" }}>{positon}</h3>
-            <p style={{ textAlign: "right" }}>{description}</p>
-          </div>
-          <div className="step">
-            <div className="outer-circle">
-              <div className="inner-circle"></div>
-            </div>
-            <hr className="vertical-line" />
-          </div>
-
-          <div className="company" style={{ textAlign: "left" }}>
-            <img src={logo} />
-            <h3>{company}</h3>
-            <p>{periodServed}</p>
-          </div>
-        </div>
-      );
-    }
-  } else {
-    return (
-      <div className="experience-div">
-        <div className="step">
-          <div className="outer-circle">
-            <div className="inner-circle"></div>
-          </div>
-          <hr className="vertical-line" />
-        </div>
-        <div className="company" style={{ textAlign: "left" }}>
-          <img src={logo} />
-          <h3>{company}</h3>
-          <p>{periodServed}</p>
-
-          <h3>{positon}</h3>
-          <p>{description}</p>
-        </div>
+  return (
+    <div className={`experience-div ${id % 2 !== 0 ? "reversed" : ""}`}>
+      <div className="responsibilities" style={{}}>
+        <h3>{positon}</h3>
+        <p>{description}</p>
       </div>
-    );
-  }
+      <div className="step">
+        <div className="outer-circle">
+          <div className="inner-circle"></div>
+        </div>
+        <hr className="vertical-line" />
+      </div>
+
+      <div className="company">
+        <img src={logo} />
+        <h3>{company}</h3>
+        <p>{periodServed}</p>
+      </div>
+    </div>
+  );
 }
